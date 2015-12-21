53dcade6ff828cc764557027dc7112c0fb71aef2
8613480677352 <nl79795905@gmail.com>
2015-12-21 11:21:57 +0800

    项目第一次提交，测试信息看下

---
+++ firstGit/firstGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstGit
+//
+//  Created by apple on 15/12/21.
+//  Copyright © 2015年 yunjiazhineng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstGit/firstGit/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  firstGit
+//
+//  Created by apple on 15/12/21.
+//  Copyright © 2015年 yunjiazhineng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"这是git第一个项目");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
