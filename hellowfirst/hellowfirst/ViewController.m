0d98448f281b791a0856f7dbc4671760f814251f
8613480677352 <nl79795905@gmail.com>
2015-12-21 15:17:13 +0800

    第一次提交代码

---
+++ hellowfirst/hellowfirst/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  hellowfirst
+//
+//  Created by apple on 15/12/21.
+//  Copyright © 2015年 yunjiazhineng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"this is the first code");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
